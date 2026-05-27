df17dea68e07f5627cc107e53bdf8dfb914e857a
Liz Sinclair <theoriginallizard@148fae97-2916-4695-8eea-55f344a693a0.fritz.box>
2026-05-27 13:52:29 +0200

    feat: add S3 bucket for avatars

---
+++ infrastructure/s3.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "avatars" {
+  bucket = "grocerymate-avatars-liz"
+
+  tags = {
+    Name        = "grocerymate-avatars"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "avatars" {
+  bucket = aws_s3_bucket.avatars.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "avatars" {
+  bucket = aws_s3_bucket.avatars.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
